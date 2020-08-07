57e7a209fd9e82f35df8c82a83857fdf04807efc
lucas baleeiro <lucasdominato2@gmail.com>
2020-08-07 15:36:05 -0300

    added test for ue

---
+++ ue/registration.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"free5gc/lib/ngap/ngapSctp"
+	"git.cs.nctu.edu.tw/calee/sctp"
+	"net"
+)
+
+func connectToAmf(amfIP, ranIP string, amfPort, ranPort int) (*sctp.SCTPConn, error) {
+	amfAddr, ranAddr, err := getNgapIp(amfIP, ranIP, amfPort, ranPort)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := sctp.DialSCTP("sctp", ranAddr, amfAddr)
+	if err != nil {
+		return nil, err
+	}
+	info, _ := conn.GetDefaultSentParam()
+	info.PPID = ngapSctp.NGAP_PPID
+	err = conn.SetDefaultSentParam(info)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func getNgapIp(amfIP, ranIP string, amfPort, ranPort int) (amfAddr, ranAddr *sctp.SCTPAddr, err error) {
+	ips := []net.IPAddr{}
+	// se der um erro != nill entra no if.
+	if ip, err1 := net.ResolveIPAddr("ip", amfIP); err1 != nil {
+		err = fmt.Errorf("Error resolving address '%s': %v", amfIP, err1)
+		return
+	} else {
+		ips = append(ips, *ip)
+	}
+	amfAddr = &sctp.SCTPAddr{
+		IPAddrs: ips,
+		Port:    amfPort,
+	}
+	ips = []net.IPAddr{}
+	if ip, err1 := net.ResolveIPAddr("ip", ranIP); err1 != nil {
+		err = fmt.Errorf("Error resolving address '%s': %v", ranIP, err1)
+		return
+	} else {
+		ips = append(ips, *ip)
+	}
+	ranAddr = &sctp.SCTPAddr{
+		IPAddrs: ips,
+		Port:    ranPort,
+	}
+	return
+}
+
+func connectToUpf(enbIP, upfIP string, gnbPort, upfPort int) (*net.UDPConn, error) {
+	upfAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", upfIP, upfPort))
+	if err != nil {
+		return nil, err
+	}
+	gnbAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", enbIP, gnbPort))
+	if err != nil {
+		return nil, err
+	}
+	return net.DialUDP("udp", gnbAddr, upfAddr)
+}
