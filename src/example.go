b32d888e403f4c2f994325168a1db8fd60c763b3
lucas baleeiro <lucasdominato2@gmail.com>
2020-08-24 00:01:30 -0300

    added testing multiple UEs as function and removed hardcode on suci UE.

---
+++ src/example.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// testing authentication for a GNB
+	fmt.Println(testAttachGnb())
+
+	// testing attach and ping with 80 UEs.
+	fmt.Println(testMultiAttachUes(80))
+
+}
