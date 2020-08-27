01b86c1cf93cace835d4e4fb40299f2b03781ddf
lucas baleeiro <lucasdominato2@gmail.com>
2020-08-27 00:21:59 -0300

    added the registration test of several UEs in parallel

--- src/example.go
+++ src/example.go
@@ -5,9 +5,12 @@ import "fmt"
 func main() {
 
 	// testing authentication for a GNB
-	fmt.Println(testAttachGnb())
+	//fmt.Println(testAttachGnb())
 
 	// testing attach and ping with 80 UEs.
-	fmt.Println(testMultiAttachUes(80))
+	//fmt.Println(testMultiAttachUes(80))
+
+	// testing UEs registration in parallel.
+	fmt.Println(testMultiAttachUesInConcurrency())
 
 }
--- src/tests.go
+++ src/tests.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // testing attach and ping for a UE.
 func testAttachUe() error {
@@ -130,6 +133,119 @@ func testMultiAttachUes(numberUes int) error {
 	// end sockets.
 	conn.Close()
 	upfConn.Close()
+
+	return nil
+}
+
+func testMultiAttachUesInConcurrency() error {
+	const ranIpAddr string = "10.200.200.2"
+	const ran2IpAddr string = "10.200.200.1"
+
+	var wg sync.WaitGroup
+
+	// make N2(RAN connect to AMF)
+	conn, err := connectToAmf("127.0.0.1", "127.0.0.1", 38412, 9487)
+	if err != nil {
+		return fmt.Errorf("The test failed when sctp socket tried to connect to AMF! Error:%s", err)
+	}
+
+	// make n3(RAN connect to UPF)
+	//upfConn, err := connectToUpf(ranIpAddr, "10.200.200.102", 2152, 2152)
+	//if err != nil {
+	//	return fmt.Errorf("The test failed when udp socket tried to connect to UPF! Error:%s", err)
+	//}
+
+	// make N2(RAN2 connect to AMF)
+	conn2, err := connectToAmf("127.0.0.1", "127.0.0.1", 38412, 9488)
+	if err != nil {
+		return fmt.Errorf("The test failed when sctp socket 2 tried to connect to AMF! Error:%s", err)
+	}
+
+	// make n3(RAN2 connect to UPF)
+	//upfConn2, err := connectToUpf(ranIpAddr2, "10.200.200.102", 2152, 2152)
+	//if err != nil {
+	//	return fmt.Errorf("The test failed when udp socket tried to connect to UPF! Error:%s", err)
+	//}
+
+	// authentication to a GNB1.
+	err = registrationGNB(conn, []byte("\x00\x01\x02"), "free5gc")
+	if err != nil {
+		return fmt.Errorf("The test failed when GNB tried to attach! Error:%s", err)
+	}
+
+	// authentication to a GNB2.
+	err = registrationGNB(conn2, []byte("\x00\x01\x01"), "free5gc2")
+	if err != nil {
+		return fmt.Errorf("The test failed when GNB tried to attach! Error:%s", err)
+	}
+
+	// authentication and ping to some UEs in parallel.
+
+	// Launch several goroutines and increment the WaitGroup counter for each.
+	wg.Add(1)
+
+	// goroutine.
+	go func(wg *sync.WaitGroup) {
+
+		defer wg.Done()
+
+		for i := 1; i <= 2; i++ {
+			// generating some IMSIs to each UE.
+			imsi := generateImsi(i)
+
+			// authentication to a UE.
+			suciv1, suciv2, err := decodeUeSuci(imsi)
+			if err != nil {
+				fmt.Println("The test failed when SUCI was created! Error:%s in Thread with imsi:%s", err, imsi)
+			}
+
+			err = registrationUE(conn, imsi, int64(i), suciv2, suciv1, ranIpAddr)
+			if err != nil {
+				fmt.Println("The test failed when UE tried to attach! Error:%s in Thread with imsi:%s", err, imsi)
+			}
+			// thread worked fine.
+			fmt.Println("Thread with imsi:%s worked fine", imsi)
+		}
+
+	}(&wg)
+
+	// increment the WaitGroup counter.
+	wg.Add(1)
+
+	// goroutine.
+	go func(wg *sync.WaitGroup) {
+
+		defer wg.Done()
+
+		for i := 3; i <= 4; i++ {
+			// generating some IMSIs to each UE.
+			imsi := generateImsi(i)
+
+			// authentication to a UE.
+			suciv1, suciv2, err := decodeUeSuci(imsi)
+			if err != nil {
+				fmt.Println("The test failed when SUCI was created! Error:%s in Thread with imsi:%s", err, imsi)
+			}
+
+			err = registrationUE(conn2, imsi, int64(i), suciv2, suciv1, ran2IpAddr)
+			if err != nil {
+				fmt.Println("The test failed when UE tried to attach! Error:%s in Thread with imsi:%s", err, imsi)
+			}
+
+			// thread worked fine.
+			fmt.Println("Thread with imsi:%s worked fine", imsi)
+		}
+
+	}(&wg)
+
+	// wait for multiple goroutines.
+	wg.Wait()
+
+	// end sockets.
+	conn.Close()
+	conn2.Close()
+	// upfConn.Close()
+	// upfConn2.Close()
 
 	return nil
 }
