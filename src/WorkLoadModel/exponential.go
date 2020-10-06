128e0050fcde4f16f6f568790faa73a5b976867e
lucas baleeiro <lucasdominato2@gmail.com>
2020-10-06 14:33:17 -0300

    added workload model with the poisson,gaussian and exponential distribution

---
+++ src/WorkLoadModel/exponential.go
@@ -0,0 +1,38 @@
+package WorkLoadModel
+
+/*
+#cgo CFLAGS: -I /home/lucas-baleeiro/gsl/include
+#cgo LDFLAGS: -L /home/lucas-baleeiro/gsl/lib -lgsl -lgslcblas -lm
+#include<gsl/gsl_rng.h>
+#include<gsl/gsl_randist.h>
+
+unsigned int randomNumberFromExponential(double mu, long seed){
+	const gsl_rng_type* T;
+    gsl_rng* r;
+    unsigned int dist;
+
+	gsl_rng_env_setup();
+	T = gsl_rng_default;
+	r = gsl_rng_alloc(T);
+
+    gsl_rng_set(r,seed);
+    dist = gsl_ran_exponential (r, mu);
+
+	gsl_rng_free (r);
+
+	return dist;
+}
+*/
+import "C"
+
+// calculate some random numbers from the Exponential distribution
+func exponentialDistribution(mean float64, length int, const_seed int) (distExpo []uint) {
+	distExpo = make([]uint, length)
+
+	for i := 1; i <= length; i++ {
+		seed := const_seed + i
+		aux := C.randomNumberFromExponential(C.double(mean), C.long(seed))
+		distExpo[i-1] = uint(aux)
+	}
+	return
+}
---
+++ src/WorkLoadModel/gaussian.go
@@ -0,0 +1,38 @@
+package WorkLoadModel
+
+/*
+#cgo CFLAGS: -I /home/lucas-baleeiro/gsl/include
+#cgo LDFLAGS: -L /home/lucas-baleeiro/gsl/lib -lgsl -lgslcblas -lm
+#include<gsl/gsl_rng.h>
+#include<gsl/gsl_randist.h>
+
+unsigned int randomNumberFromGaussian(double mu, long seed){
+	const gsl_rng_type* T;
+    gsl_rng* r;
+    unsigned int dist;
+
+	gsl_rng_env_setup();
+	T = gsl_rng_default;
+	r = gsl_rng_alloc(T);
+
+    gsl_rng_set(r,seed);
+    dist = gsl_ran_gaussian (r, mu);
+
+	gsl_rng_free (r);
+
+	return dist;
+}
+*/
+import "C"
+
+// calculate some random numbers from the Gaussian distribution
+func gaussianlDistribution(sigma float64, length int, const_seed int) (distGaussian []uint) {
+	distGaussian = make([]uint, length)
+
+	for i := 1; i <= length; i++ {
+		seed := const_seed + i
+		aux := C.randomNumberFromGaussian(C.double(sigma), C.long(seed))
+		distGaussian[i-1] = uint(aux)
+	}
+	return
+}
