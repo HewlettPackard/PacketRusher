081320ed4feac22db296a9ef14bb3c8f394332b3
lucas baleeiro <lucasdominato2@gmail.com>
2022-07-31 22:58:47 -0300

    added modified version of init UE registration for UE latency test

--- internal/templates/test-ue-latency-in-interval.go
+++ internal/templates/test-ue-latency-in-interval.go
@@ -3,6 +3,8 @@ package templates
 import (
 	log "github.com/sirupsen/logrus"
 	"my5G-RANTester/config"
+	"my5G-RANTester/internal/control_test_engine/gnb"
+	"my5G-RANTester/internal/control_test_engine/ue"
 	"my5G-RANTester/internal/monitoring"
 	"sync"
 	"time"
@@ -23,6 +25,7 @@ func TestUesLatencyInInterval(interval int) int64 {
 		log.Fatal("Error in get configuration")
 	}
 
+	// creates an sctp socket (GNB) for each UE
 	for i := 1; i <= interval; i++ {
 
 		// sinal da gnb para manter a execução
@@ -30,7 +33,7 @@ func TestUesLatencyInInterval(interval int) int64 {
 		synch := make(chan bool, 1)
 
 		// usado para sincronizar se a thread da gnb gerou um erro
-		go gnb.InitGnb2(cfg, sigGnb, synch)
+		go gnb.InitGnbForUeLatency(cfg, sigGnb, synch)
 
 		// não houve erro na gnb
 		if <-synch {
@@ -49,9 +52,6 @@ func TestUesLatencyInInterval(interval int) int64 {
 		} else {
 			log.Warn("[TESTER][UE] UE LATENCY IN REGISTRATION: WITHOUT CONNECTION")
 		}
-
-		// ue registration per second
-		time.Sleep(600 * time.Millisecond)
 
 		// seta o sinal e termina a gnb
 		sigGnb <- true
