b671f1753e737c39e74822f5432cfc8ac9ea6a85
lucas baleeiro <lucasdominato2@gmail.com>
2022-07-31 22:29:00 -0300

    added template for Test latency of UE registration

---
+++ internal/templates/test-ue-latency-in-interval.go
@@ -0,0 +1,63 @@
+package templates
+
+import (
+	log "github.com/sirupsen/logrus"
+	"my5G-RANTester/config"
+	"my5G-RANTester/internal/monitoring"
+	"sync"
+	"time"
+)
+
+// gera uma UE registration e mede a latência
+func TestUesLatencyInInterval(interval int) int64 {
+
+	wg := sync.WaitGroup{}
+
+	monitor := monitoring.Monitor{
+		LtRegisterGlobal: 0,
+	}
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		//return nil
+		log.Fatal("Error in get configuration")
+	}
+
+	for i := 1; i <= interval; i++ {
+
+		// sinal da gnb para manter a execução
+		sigGnb := make(chan bool, 1)
+		synch := make(chan bool, 1)
+
+		// usado para sincronizar se a thread da gnb gerou um erro
+		go gnb.InitGnb2(cfg, sigGnb, synch)
+
+		// não houve erro na gnb
+		if <-synch {
+
+			time.Sleep(400 * time.Millisecond)
+
+			log.Warn("[TESTER][UE] Test UE REGISTRATION:")
+			go ue.RegistrationUeMonitor(cfg, uint8(i), &monitor, &wg)
+
+			wg.Add(1)
+
+			wg.Wait()
+
+			// increment the latency global for the mean
+			monitor.SetLtGlobal(monitor.LtRegisterLocal)
+		} else {
+			log.Warn("[TESTER][UE] UE LATENCY IN REGISTRATION: WITHOUT CONNECTION")
+		}
+
+		// ue registration per second
+		time.Sleep(600 * time.Millisecond)
+
+		// seta o sinal e termina a gnb
+		sigGnb <- true
+
+		time.Sleep(40 * time.Millisecond)
+	}
+
+	return monitor.LtRegisterGlobal
+}
