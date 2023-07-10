9666e115781e13385bb0ec19fb905db9d5df214b
Valentin D'Emmanuele <valentin.demmanuele@hpe.com>
2023-07-10 14:22:32 +0000

    Create Route and Rules automatically, as well as clean them up on CTRL-C

--- internal/control_test_engine/ue/gtp/service/service.go
+++ internal/control_test_engine/ue/gtp/service/service.go
@@ -3,16 +3,17 @@ package service
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
-	"my5G-RANTester/internal/control_test_engine/ue/context"
-    gnbContext 	"my5G-RANTester/internal/control_test_engine/gnb/context"
+	"github.com/vishvananda/netlink"
 	gtpLink "my5G-RANTester/internal/cmd/gogtp5g-link"
 	gtpTunnel "my5G-RANTester/internal/cmd/gogtp5g-tunnel"
-	"github.com/vishvananda/netlink"
+	gnbContext "my5G-RANTester/internal/control_test_engine/gnb/context"
+	"my5G-RANTester/internal/control_test_engine/ue/context"
 
-    "encoding/json"
+	"encoding/json"
 	"net"
-	"time"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func SetupGtpInterface(ue *context.UEContext, message []byte) {
@@ -70,11 +71,12 @@ func SetupGtpInterface(ue *context.UEContext, message []byte) {
         return
     }
 
+    netUeIp := net.ParseIP(ueIp)
 	// add an IP address to a link device.
 	addrTun := &netlink.Addr{
 		IPNet: &net.IPNet{
-			IP:   net.ParseIP(ueIp).To4(),
-			Mask: net.IPv4Mask(255, 255, 255, 0),
+			IP:   netUeIp.To4(),
+			Mask: net.IPv4Mask(255, 255, 255, 255),
 		},
 	}
 
@@ -86,28 +88,39 @@ func SetupGtpInterface(ue *context.UEContext, message []byte) {
 		return
 	}
 
-    log.Info(fmt.Sprintf("[GNB][GTP] Interface %s has successfully been configured for UE %s", nameInf, ueIp))
+	tableId, _ := strconv.Atoi(msg.GetOTeid())
 
-/*
 	route := &netlink.Route{
-		Dst:       &net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 32)}, // default
-		LinkIndex: gnb.GetN3Plane().KernelGTP.Link.Attrs().Index,            // dev gtp-<ECI>
+		Dst:       &net.IPNet{IP: net.ParseIP("30.103.12.0").To4(), Mask: net.CIDRMask(24, 32)}, // default
+		LinkIndex: link.Attrs().Index,            // dev gtp-<ECI>
 		Scope:     netlink.SCOPE_LINK,                                      // scope link
 		Protocol:  4,                                                       // proto static
 		Priority:  1,                                                       // metric 1
-		Table:     1001,                                                    // table <ECI>
+		Table:     tableId,                                                    // table <ECI>
 	}
 
-	if netlink.RouteReplace(route) != nil {
+	if err := netlink.RouteReplace(route); err != nil {
 	   log.Fatal("[GNB][GTP] Unable to create Kernel Route ", err)
 	}
+	ue.SetTunRoute(route)
 
     rule := netlink.NewRule()
-    rule.IifName = gnb.GetN3Plane().KernelGTP.Link.Name
-    rule.Table = 1001
+    rule.IifName = nameInf
+    rule.Table = tableId
 
     if err := netlink.RuleAdd(rule); err != nil {
 	   log.Fatal("[GNB][GTP] Unable to create Kernel Route ", err)
-    }*/
+    }
 
+	rule = netlink.NewRule()
+	rule.IifName = nameInf
+	rule.Src = &net.IPNet{IP: netUeIp, Mask: net.CIDRMask(0, 32)}
+	rule.Table = tableId
+
+	if err := netlink.RuleAdd(rule); err != nil {
+		log.Fatal("[GNB][GTP] Unable to create Kernel Route ", err)
+	}
+	ue.SetTunRule(rule)
+
+	log.Info(fmt.Sprintf("[GNB][GTP] Interface %s has successfully been configured for UE %s", nameInf, ueIp))
 }
