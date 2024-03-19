6e163a5385fd9534338909bc4c7cc20b97543bbd
brianchennn <ny40ny40ny.cs11@nycu.edu.tw>
2024-03-19 08:11:17 +0000

    fix: use SecurityHeaderTypeIntegrityProtectedAndCiphered as Security Header Type in Configuration Update Complete message
    Signed-off-by: brianchennn <ny40ny40ny.cs11@nycu.edu.tw>

--- internal/control_test_engine/ue/nas/message/nas_control/mm_5gs/configuration-update-complete.go
+++ internal/control_test_engine/ue/nas/message/nas_control/mm_5gs/configuration-update-complete.go
@@ -7,19 +7,31 @@ package mm_5gs
 import (
 	"bytes"
 	"fmt"
+	"my5G-RANTester/internal/control_test_engine/ue/context"
+	"my5G-RANTester/internal/control_test_engine/ue/nas/message/nas_control"
+
 	"github.com/free5gc/nas"
 	"github.com/free5gc/nas/nasMessage"
-	"my5G-RANTester/internal/control_test_engine/ue/context"
 )
 
-func ConfigurationUpdateComplete(ue *context.UEContext) (nasPdu []byte) {
+func ConfigurationUpdateComplete(ue *context.UEContext) ([]byte, error) {
+	pdu := getConfigurationUpdateComplete()
+	pdu, err := nas_control.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding %s IMSI UE NAS Registration Complete Msg", ue.UeSecurity.Supi)
+	}
+
+	return pdu, nil
+}
+
+func getConfigurationUpdateComplete() (nasPdu []byte) {
 	m := nas.NewMessage()
 	m.GmmMessage = nas.NewGmmMessage()
 	m.GmmHeader.SetMessageType(nas.MsgTypeConfigurationUpdateComplete)
 
 	configurationUpdateComplete := nasMessage.NewConfigurationUpdateComplete(0)
 	configurationUpdateComplete.SetExtendedProtocolDiscriminator(nasMessage.Epd5GSMobilityManagementMessage)
-	configurationUpdateComplete.SetSecurityHeaderType(0x00)
+	configurationUpdateComplete.SetSecurityHeaderType(nas.SecurityHeaderTypeIntegrityProtectedAndCiphered)
 	configurationUpdateComplete.SetSpareHalfOctet(0x00)
 	configurationUpdateComplete.SetMessageType(nas.MsgTypeConfigurationUpdateComplete)
 
