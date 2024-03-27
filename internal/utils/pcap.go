ddf64fb17f0a388fb25db151b22e3c86fd21161a
brianchennn <ny40ny40ny.cs11@nycu.edu.tw>
2024-03-27 07:15:55 +0000

    support multi-AMFs N2 Connection
    Signed-off-by: brianchennn <ny40ny40ny.cs11@nycu.edu.tw>

--- internal/utils/pcap.go
+++ internal/utils/pcap.go
@@ -25,30 +25,35 @@ func CaptureTraffic(path cli.Path) {
 	}
 
 	config := config.GetConfig()
-	ip := net.ParseIP(config.AMF.Ip)
-	route, err := netlink.RouteGet(ip)
-	if err != nil || len(route) <= 0 {
-		log.Fatal("Unable to capture traffic to AMF as we are unable to determine route to AMF. Aborting.", err)
-	}
-
-	iif, err := netlink.LinkByIndex(route[0].LinkIndex)
-
-	pcapw := pcapgo.NewWriter(f)
-	if err := pcapw.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
-		log.Fatalf("WriteFileHeader: %v", err)
-	}
-
-	handle, err := pcapgo.NewEthernetHandle(iif.Attrs().Name)
-	if err != nil {
-		log.Fatalf("OpenEthernet: %v", err)
-	}
-
-	pkgsrc := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
-	go func() {
-		for packet := range pkgsrc.Packets() {
-			if err := pcapw.WritePacket(packet.Metadata().CaptureInfo, packet.Data()); err != nil {
-				log.Fatalf("pcap.WritePacket(): %v", err)
-			}
+	for _, amf := range config.AMFs {
+		ip := net.ParseIP(amf.Ip)
+		route, err := netlink.RouteGet(ip)
+		if err != nil || len(route) <= 0 {
+			log.Fatal("Unable to capture traffic to AMF as we are unable to determine route to AMF. Aborting.", err)
 		}
-	}()
+
+		iif, err := netlink.LinkByIndex(route[0].LinkIndex) 
+		if err != nil {
+			log.Fatalf("LinkByIndex: %v", err)
+		}
+
+		pcapw := pcapgo.NewWriter(f)
+		if err := pcapw.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
+			log.Fatalf("WriteFileHeader: %v", err)
+		}
+
+		handle, err := pcapgo.NewEthernetHandle(iif.Attrs().Name)
+		if err != nil {
+			log.Fatalf("OpenEthernet: %v", err)
+		}
+
+		pkgsrc := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+		go func() {
+			for packet := range pkgsrc.Packets() {
+				if err := pcapw.WritePacket(packet.Metadata().CaptureInfo, packet.Data()); err != nil {
+					log.Fatalf("pcap.WritePacket(): %v", err)
+				}
+			}
+		}()
+	}
 }
