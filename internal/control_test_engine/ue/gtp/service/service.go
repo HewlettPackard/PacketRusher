d2543bebff8be55242ce819938e10a471bfb4bb5
Junxiao Shi <git@mail1.yoursunny.com>
2025-03-31 16:23:01 +0000

    fix: set outer source IP for uplink GTP-U traffic
    fixes #164
    
    Signed-off-by: Junxiao Shi <git@mail1.yoursunny.com>

--- internal/control_test_engine/ue/gtp/service/service.go
+++ internal/control_test_engine/ue/gtp/service/service.go
@@ -72,70 +72,99 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 
 	time.Sleep(time.Second)
 
-	cmdAddFar := []string{nameInf, "1", "--action", "2"}
+	// Create FAR for uplink.
+	cmdAddFar := []string{nameInf,
+		"1",             // FAR ID = 1
+		"--action", "2", // Apply Action = FORW
+	}
 	log.Debug("[UE][GTP] Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
 	if err := gtpTunnel.CmdAddFAR(cmdAddFar); err != nil {
 		log.Fatal("[GNB][GTP] Unable to create FAR: ", err)
 		return
 	}
 
-	cmdAddFar = []string{nameInf, "2", "--action", "2", "--hdr-creation", "0", fmt.Sprint(gnbPduSession.GetTeidUplink()), upfIp, "2152"}
+	// Create FAR for downlink.
+	cmdAddFar = []string{nameInf,
+		"2",             // FAR ID = 2
+		"--action", "2", // Apply Action = FORW
+		"--hdr-creation", "0", strconv.FormatUint(uint64(gnbPduSession.GetTeidUplink()), 10), upfIp, "2152", // Outer Header Creation
+	}
 	log.Debug("[UE][GTP] Setting up GTP Forwarding Action Rule for ", strings.Join(cmdAddFar, " "))
 	if err := gtpTunnel.CmdAddFAR(cmdAddFar); err != nil {
 		log.Fatal("[UE][GTP] Unable to create FAR ", err)
 		return
 	}
 
-	cmdAddPdr := []string{nameInf, "1", "--pcd", "1", "--hdr-rm", "0", "--ue-ipv4", ueIp, "--f-teid",
-		fmt.Sprint(gnbPduSession.GetTeidDownlink()), msg.GnbIp, "--far-id", "1", "--src-intf", "1"}
+	// Create PDR for uplink.
+	cmdAddPdr := []string{nameInf,
+		"1",          // PDR ID = 1
+		"--pcd", "1", // Precedence = 1
+		"--hdr-rm", "0", // Outer Header Removal = GTP-U/UDP/IPv4
+		"--ue-ipv4", ueIp, // UE IP Address
+		"--f-teid", strconv.FormatUint(uint64(gnbPduSession.GetTeidDownlink()), 10), msg.GnbIp, // F-TEID
+		"--far-id", "1", // FAR ID = 1
+		"--src-intf", "1", // Source Interface = Core
+	}
 	log.Debug("[UE][GTP] Setting up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
-
 	if err := gtpTunnel.CmdAddPDR(cmdAddPdr); err != nil {
 		log.Fatal("[GNB][GTP] Unable to create FAR: ", err)
 		return
 	}
 
-	cmdAddPdr = []string{nameInf, "2", "--pcd", "2", "--ue-ipv4", ueIp, "--far-id", "2", "--src-intf", "0"}
-	cmdAddQer := []string{nameInf, "1", "--qfi", strconv.Itoa(int(qfi))}
+	cmdAddPdr = []string{nameInf,
+		"2",          // PDR ID = 2
+		"--pcd", "2", // Precedence = 2
+		"--ue-ipv4", ueIp, // UE IP Address
+		"--far-id", "2", // FAR ID = 2
+		"--src-intf", "0", // Source Interface = Access
+		"--gtpu-src-ip", ueGnbIp.String(), // GTP-U source IP address (not part of PFCP spec)
+	}
 	if qfi > 0 {
+		// Create QER for downlink.
+		cmdAddQer := []string{nameInf,
+			"1",                                 // QER ID = 1
+			"--qfi", strconv.FormatInt(qfi, 10), // QFI
+		}
 		log.Debug("[UE][GTP] Setting Up QFI", strings.Join(cmdAddQer, " "))
 		if err := gtpTunnel.CmdAddQER(cmdAddQer); err != nil {
 			log.Fatal("[UE][GTP] Unable to create QER:", err)
 			return
 		}
-		cmdAddPdr = append(cmdAddPdr, "--qer-id", "1")
+		cmdAddPdr = append(cmdAddPdr,
+			"--qer-id", "1", // QER ID = 1
+		)
 	}
 
+	// Create PDR for downlink.
 	log.Debug("[UE][GTP] Setting Up GTP Packet Detection Rule for ", strings.Join(cmdAddPdr, " "))
 	if err := gtpTunnel.CmdAddPDR(cmdAddPdr); err != nil {
 		log.Fatal("[UE][GTP] Unable to create FAR ", err)
 		return
 	}
 
-	netUeIp := net.ParseIP(ueIp)
-	// add an IP address to a link device.
-	addrTun := &netlink.Addr{
-		IPNet: &net.IPNet{
-			IP:   netUeIp.To4(),
-			Mask: net.IPv4Mask(255, 255, 255, 255),
-		},
-	}
-
+	// Find TUN network interface.
 	link, _ := netlink.LinkByName(nameInf)
 	pduSession.SetTunInterface(link)
 
+	// Add UE IP Address onto the TUN network interface.
+	addrTun := &netlink.Addr{
+		IPNet: &net.IPNet{
+			IP:   net.ParseIP(ueIp).To4(),
+			Mask: net.IPv4Mask(255, 255, 255, 255),
+		},
+	}
 	if err := netlink.AddrAdd(link, addrTun); err != nil {
 		log.Fatal("[UE][DATA] Error in adding IP for virtual interface", err)
 		return
 	}
 
-	tableId, _ := strconv.Atoi(fmt.Sprint(gnbPduSession.GetTeidUplink()))
-
+	// Configure routing policy or VRF for the UE.
+	tableId := gnbPduSession.GetTeidUplink()
 	switch ue.TunnelMode {
 	case config.TunnelTun:
 		rule := netlink.NewRule()
 		rule.Priority = 100
-		rule.Table = tableId
+		rule.Table = int(tableId)
 		rule.Src = addrTun.IPNet
 		_ = netlink.RuleDel(rule)
 
@@ -149,7 +178,7 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 			LinkAttrs: netlink.LinkAttrs{
 				Name: vrfInf,
 			},
-			Table: uint32(tableId),
+			Table: tableId,
 		}
 		_ = netlink.LinkDel(vrfDevice)
 
@@ -170,15 +199,15 @@ func SetupGtpInterface(ue *context.UEContext, msg gnbContext.UEMessage) {
 		pduSession.SetVrfDevice(vrfDevice)
 	}
 
+	// Insert default route from the UE to the Data Network.
 	route := &netlink.Route{
 		Dst:       &net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 32)}, // default
 		LinkIndex: link.Attrs().Index,                                      // dev val<MSIN>
 		Scope:     netlink.SCOPE_LINK,                                      // scope link
 		Protocol:  4,                                                       // proto static
 		Priority:  1,                                                       // metric 1
-		Table:     tableId,                                                 // table <ECI>
+		Table:     int(tableId),                                            // table <ECI>
 	}
-
 	if err := netlink.RouteReplace(route); err != nil {
 		log.Fatal("[GNB][GTP] Unable to create Kernel Route ", err)
 	}
